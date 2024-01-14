37df85e92b3c20dbd68c3bbe09821408a8923152
Martin Vigl <martinvigl06@gmail.com>
2024-01-14 22:09:31 +0100

    Serialize work but not done

---
+++ cmake-build-debug/CMakeFiles/Trie.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trie.dir/main.c.obj"
+  "CMakeFiles/Trie.dir/main.c.obj.d"
+  "CMakeFiles/Trie.dir/trie.c.obj"
+  "CMakeFiles/Trie.dir/trie.c.obj.d"
+  "Trie.exe"
+  "Trie.exe.manifest"
+  "Trie.lib"
+  "Trie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Trie.dir/main.c.obj.d
@@ -0,0 +1,10 @@
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.37.32822/include/vcruntime.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.37.32822/include/sal.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.37.32822/include/concurrencysal.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.37.32822/include/vadefs.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_stdio_config.h
+C:/Users/gusta/CLionProjects/Trie-in-C/trie.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.37.32822/include/stdbool.h
